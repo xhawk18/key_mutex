d70b4a439133280224c837727ea185ce66f2affe
XH Chen <harrychen@chainresearch.org>
2017-05-05 11:36:57 +0800

    add testing program.

---
+++ test_await.js
@@ -0,0 +1,78 @@
+var rw_mutex = require('./rw_mutex');
+
+async function delay(ms){
+    return await new Promise(function(resolve){
+        setTimeout(resolve, ms);
+    });
+}
+
+async function test(){
+
+    var l0 = rw_mutex.asyncMutex();
+    
+    l0.rlock(async()=>{
+        console.log('A: read step 0');
+        await delay(50);
+        console.log('A: read step 1');
+        await delay(50);
+        console.log('A: read step 2');
+        await delay(50);
+    });
+
+    l0.rlock(async()=>{
+        console.log('B: read step 0');
+        await delay(50);
+        console.log('B: read step 1');
+        await delay(50);
+        console.log('B: read step 2');
+        await delay(50);
+    });     
+
+    (async()=> {
+        try{
+            await l0.wlock(async()=>{
+                console.log('C: write step 0');
+                await delay(50);
+                console.log('C: write step 1');
+                throw new Error('I throw an error here!');
+                await delay(50);
+                console.log('C: write step 2');
+                await delay(50);
+            });
+        }catch(err){
+            console.log(err);
+        }
+    })();
+
+    /* await */ l0.wlock(async()=>{
+        console.log('D: write step 0');
+        await delay(50);
+        console.log('D: write step 1');
+        await delay(50);
+        console.log('D: write step 2');
+        await delay(50);
+    });
+
+
+
+    l0.rlock(async()=>{
+        console.log('E: read step 0');
+        await delay(50);
+        console.log('E: read step 1');
+        await delay(50);
+        console.log('E: read step 2');
+        await delay(50);
+    });
+
+    l0.rlock(async()=>{
+        console.log('F: read step 0');
+        await delay(50);
+        console.log('F: read step 1');
+        await delay(50);
+        console.log('F: read step 2');
+        await delay(50);
+    });
+}
+
+test();
+
---
+++ test_promise.js
@@ -0,0 +1,86 @@
+var rw_mutex = require('./rw_mutex');
+
+function delay(ms){
+    return new Promise(function(resolve){
+        setTimeout(resolve, ms);
+    });
+}
+
+function test(){
+
+    var l0 = rw_mutex.asyncMutex();
+    
+    l0.rlock(function(){
+        console.log('A: read step 0');
+        return delay(50).then(function(){
+            console.log('A: read step 1');
+            return delay(50);
+        }).then(function(){
+            console.log('A: read step 2');
+            return delay(50);
+        });
+    });
+
+    l0.rlock(function(){
+        console.log('B: read step 0');
+        return delay(50).then(function(){
+            console.log('B: read step 1');
+            return delay(50);
+        }).then(function(){
+            console.log('B: read step 2');
+            return delay(50);
+        });
+    });
+
+    l0.wlock(function(){
+        console.log('C: write step 0');
+        return delay(50).then(function(){
+            console.log('C: write step 1');
+            throw new Error('I throw an error here!');
+            return delay(50);
+        }).then(function(){
+            console.log('C: write step 2');
+            return delay(50);
+        })['catch'](function(err){
+            console.log(err);
+        });
+    });
+
+    l0.wlock(function(){
+        console.log('D: write step 0');
+        return delay(50).then(function(){
+            console.log('D: write step 1');
+            return delay(50);
+        }).then(function(){
+            console.log('D: write step 2');
+            return delay(50);
+        });
+    });
+
+
+
+    l0.rlock(function(){
+        console.log('E: read step 0');
+        return delay(50).then(function(){
+            console.log('E: read step 1');
+            return delay(50);
+        }).then(function(){
+            console.log('E: read step 2');
+            return delay(50);
+        });
+    });
+
+    l0.rlock(function(){
+        console.log('F: read step 0');
+        return delay(50).then(function(){
+            console.log('F: read step 1');
+            return delay(50);
+        }).then(function(){
+            console.log('F: read step 2');
+            return delay(50);
+        });
+    });
+}
+
+test();
+
